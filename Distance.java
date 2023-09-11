cf3200b15533403c0cdb3c71eee7c78b72390dae
Yigit Ceren <104066608+Yyigit23@users.noreply.github.com>
2023-09-12 01:18:14 +0300

    Add files via upload

---
+++ Distance.java
@@ -0,0 +1,13 @@
+
+public class Distance {
+    public static void main(String args[]) {
+      
+    
+      
+      
+
+      System.out.println("Hello world");
+      System.out.println("Selam Dünya");
+
+    }
+}
